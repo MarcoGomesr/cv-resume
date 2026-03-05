a5dbe05d7756ea66ce38a629e3e37a4aa79b7aa3
Marco <marcogomesr@gmail.com>
2026-03-05 19:01:33 +0100

    feat: add resume card

--- app/components/resumeCard/ResumeCard.tsx
+++ app/components/resumeCard/ResumeCard.tsx
@@ -1,44 +1,96 @@
-import { Link } from "react-router";
-import ScoreCircle from "../scoreCircle/ScoreCircle";
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router";
+import ATS from "~/components/ATS";
+import Details from "~/components/Details";
+import Summary from "~/components/Summary";
+import { usePuterStore } from "~/lib/puter";
 
-type ResumeProps = {
-	resume: Resume;
-};
+export const meta = () => [
+	{ title: "Resumind | Review " },
+	{ name: "description", content: "Detailed overview of your resume" },
+];
 
-export default function ResumeCard({
-	resume: { id, companyName, jobTitle, feedback, imagePath },
-}: ResumeProps) {
+const Resume = () => {
+	const { auth, isLoading, fs, kv } = usePuterStore();
+	const { id } = useParams();
+	const [imageUrl, setImageUrl] = useState("");
+	const [resumeUrl, setResumeUrl] = useState("");
+	const [feedback, setFeedback] = useState<Feedback | null>(null);
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (!isLoading && !auth.isAuthenticated)
+			navigate(`/auth?next=/resume/${id}`);
+	}, [isLoading]);
+
+	useEffect(() => {
+		const loadResume = async () => {
+			const resume = await kv.get(`resume:${id}`);
+
+			if (!resume) return;
+
+			const data = JSON.parse(resume);
+
+			const resumeBlob = await fs.read(data.resumePath);
+			if (!resumeBlob) return;
+
+			const pdfBlob = new Blob([resumeBlob], { type: "application/pdf" });
+			const resumeUrl = URL.createObjectURL(pdfBlob);
+			setResumeUrl(resumeUrl);
+
+			const imageBlob = await fs.read(data.imagePath);
+			if (!imageBlob) return;
+			const imageUrl = URL.createObjectURL(imageBlob);
+			setImageUrl(imageUrl);
+
+			setFeedback(data.feedback);
+			console.log({ resumeUrl, imageUrl, feedback: data.feedback });
+		};
+
+		loadResume();
+	}, [id]);
+
 	return (
-		<Link to={`/resume/${id}`} className="resume-card animate-fade-in">
-			<div className="resume-card-header">
-				<div className="flex flex-col gap-2">
-					{companyName && (
-						<h2 className="text-black! font-bold wrap-break-words">
-							{companyName}
-						</h2>
+		<main className="!pt-0">
+			<nav className="resume-nav">
+				<Link to="/" className="back-button">
+					<img src="/icons/back.svg" alt="logo" className="w-2.5 h-2.5" />
+					<span className="text-gray-800 text-sm font-semibold">
+						Back to Homepage
+					</span>
+				</Link>
+			</nav>
+			<div className="flex flex-row w-full max-lg:flex-col-reverse">
+				<section className="feedback-section bg-[url('/images/bg-small.svg') bg-cover h-[100vh] sticky top-0 items-center justify-center">
+					{imageUrl && resumeUrl && (
+						<div className="animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-wxl:h-fit w-fit">
+							<a href={resumeUrl} target="_blank" rel="noopener noreferrer">
+								<img
+									src={imageUrl}
+									className="w-full h-full object-contain rounded-2xl"
+									title="resume"
+								/>
+							</a>
+						</div>
 					)}
-					{jobTitle && (
-						<h3 className="text-lg wrap-break-words text-gray-500">
-							{jobTitle}
-						</h3>
+				</section>
+				<section className="feedback-section">
+					<h2 className="text-4xl !text-black font-bold">Resume Review</h2>
+					{feedback ? (
+						<div className="flex flex-col gap-8 animate-in fade-in duration-1000">
+							<Summary feedback={feedback} />
+							<ATS
+								score={feedback.ATS.score || 0}
+								suggestions={feedback.ATS.tips || []}
+							/>
+							<Details feedback={feedback} />
+						</div>
+					) : (
+						<img src="/images/resume-scan-2.gif" className="w-full" />
 					)}
-					{!companyName && !jobTitle && (
-						<h2 className="text-black! font-bold">Resume</h2>
-					)}
-				</div>
-				<div className="shrink-0">
-					<ScoreCircle score={feedback.overallScore} />
-				</div>
+				</section>
 			</div>
-			<div className="gradient-border animate-fade-in duration-1000">
-				<div className="w-full h-full">
-					<img
-						src={imagePath}
-						alt="resume"
-						className="w-full h-87.5 max-sm:h-50 object-cover object-top"
-					/>
-				</div>
-			</div>
-		</Link>
+		</main>
 	);
-}
+};
+export default Resume;
