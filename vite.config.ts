2e83dcd94f4f16f0cc6d03ad1f51d7d9e35c369c
Marco <marcogomesr@gmail.com>
2026-03-17 17:28:14 +0100

    chore: config path

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,7 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./app", import.meta.url)),
-    },
-  },
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+	plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
