3ebdf2b3b131909890e6d6c82485225e9ef95e2b
Marco <marcogomesr@gmail.com>
2026-03-17 13:14:46 +0100

    feat: update navbar with new logo and branding

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -3,11 +3,15 @@ import { Link } from "react-router";
 export default function Navbar() {
 	return (
 		<nav className="navbar">
-			<Link to="/" className="logo">
-				RESUMIND
+			<Link
+				to="/"
+				className=" text-gradient leading-tight tracking-[-2px] font-semibold"
+			>
+				<span className="text-[#606beb] font-bold text-2xl">CV</span>
+				<span className="font-bold text-2xl">RESUME</span>
 			</Link>
 			<Link className="primary-button w-fit" to="/upload">
-				Upload Resume
+				Upload CV
 			</Link>
 		</nav>
 	);
